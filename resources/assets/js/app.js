3c2b21625d879a48c11f5e1b1e7cea5c05e24605
Julio Salsavilca <j.salsavilca@gmail.com>
2018-09-05 00:36:20 -0500

    prueba nueva rama

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -14,6 +14,8 @@ window.Vue = require('vue');
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
+
+ 
 Vue.component('roles', require('./components/Roles.vue'));
 
 const app = new Vue({
