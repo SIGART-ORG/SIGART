e2859761ef1e5c5db1cfc644fb81f463cfe1333e
Julio Salsavilca <j.salsavilca@gmail.com>
2018-12-23 01:22:40 -0500

    [Config]
    Restructuración de archivos

--- public/home/julio/Projects/resources/dev/js/functions.js
+++ public/home/julio/Projects/resources/dev/js/functions.js
@@ -60,20 +60,20 @@
 /******/ 	__webpack_require__.p = "/";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 198);
+/******/ 	return __webpack_require__(__webpack_require__.s = 202);
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ 198:
+/***/ 202:
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(199);
+module.exports = __webpack_require__(203);
 
 
 /***/ }),
 
-/***/ 199:
+/***/ 203:
 /***/ (function(module, exports) {
 
 var clearString = function clearString(cadena) {
