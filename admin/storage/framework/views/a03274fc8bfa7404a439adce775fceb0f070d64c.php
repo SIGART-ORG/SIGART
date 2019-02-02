588274033c74bda1f148c2720257f87ff7dc5c6c
Julio Salsavilca <j.salsavilca@gmail.com>
2019-02-02 11:44:14 -0500

    Revert "[revert]"
    This reverts commit 695906be259dcb429e613ce84c34d907964a10c2.

--- admin/storage/framework/views/a03274fc8bfa7404a439adce775fceb0f070d64c.php
+++
@@ -1,5 +0,0 @@
-<?php $__env->startSection('contenido'); ?>
-    <holidays></holidays>
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('main', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
