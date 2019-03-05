d1d4b58764487306716efcc1c9a39acc765ee038
Julio Salsavilca <j.salsavilca@gmail.com>
2019-03-04 19:05:35 -0500

    [admin-log] correction log

--- admin/app/Http/Controllers/Controller.php
+++ admin/app/Http/Controllers/Controller.php
@@ -20,29 +20,31 @@ class Controller extends BaseController
             $admin = $user->name." ".$user->last_name;
             $message= $admin." ".$message;
         }
-        //print_r($optional);
+        if( is_object( $optional ) ){
+            $optional = (array) $optional;
+        }
         switch($type){
 
             case "info":
-                Log::info($message,$optional->toArray());
+                Log::info($message,$optional);
                 break;
             case "emergency":
-                Log::emergency($message,$optional->toArray());
+                Log::emergency($message,$optional);
                 break;
             case "alert":
-                Log::alert($message, $optional->toArray());
+                Log::alert($message, $optional);
                 break;
             case "error":
-                Log::error($message, $optional->toArray());
+                Log::error($message, $optional);
                 break;
             case "warning":
-                Log::warning($message, $optional->toArray());
+                Log::warning($message, $optional);
                 break;
             case "notice":
-                Log::notice($message, $optional->toArray());
+                Log::notice($message, $optional);
                 break;
             case "debug":
-                Log::debug($message, $optional->toArray());
+                Log::debug($message, $optional);
                 break;
         }
     }
