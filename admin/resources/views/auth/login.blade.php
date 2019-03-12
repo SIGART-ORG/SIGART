f75adb8077426efc3aa3821b176728bd9d75c2b5
Julio Salsavilca <j.salsavilca@gmail.com>
2019-03-12 11:27:49 -0500

    [admin] Login new template

--- admin/resources/views/auth/login.blade.php
+++ admin/resources/views/auth/login.blade.php
@@ -1,104 +1,67 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ str_replace( '_', '-', app()->getLocale() ) }}" class="fixed">
 <head>
 
-    <!-- Basic -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta charset="UTF-8">
-    <title>{{ env( 'NAME_PROJECT' ) }}</title>
-    <meta name="keywords" content="HTML5 Admin Template" />
-    <meta name="description" content="Porto Admin - Responsive HTML5 Template">
-    <meta name="author" content="okler.net">
-
-    <!-- Mobile Metas -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-
     <!-- Favicon -->
     <link rel="shortcut icon" href="{{ URL::asset( 'images/favicon.ico' ) }}">
-
-    <!-- Web Fonts  -->
-    <link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800|Shadows+Into+Light" rel="stylesheet" type="text/css">
-
-    <!-- Vendor CSS -->
-    <link rel="stylesheet" href="{{ URL::asset( 'css/bootstrap/bootstrap.min.css' ) }}" />
-    <link rel="stylesheet" href="{{ URL::asset( 'css/font-awesome/style/font-awesome.min.css' ) }}" />
-    <link rel="stylesheet" href="{{ URL::asset( 'css/magnific-popup/magnific-popup.min.css' ) }}" />
-
-    <!-- Theme CSS -->
-    <link rel="stylesheet" href="{{ URL::asset( 'css/theme.min.css' ) }}" />
-
-    <!-- Skin CSS -->
-    <link rel="stylesheet" href="{{ URL::asset( 'css/skins/default.min.css' ) }}" />
-
+    <!-- Main CSS-->
+    <link rel="stylesheet" type="text/css" href="{{ URL::asset( 'css/main.min.css' ) }}">
+    <!-- Font-icon css-->
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Login | {{ env( 'NAME_PROJECT' ) }}</title>
 </head>
 <body>
-<!-- start: page -->
-<section class="body-sign">
-    <div class="center-sign">
-        <a href="/" class="logo pull-left">
-            <img src="{{ URL::asset( 'images/logo.png' ) }}" height="54" alt="Porto Admin" />
-        </a>
-
-        <div class="panel panel-sign">
-            <div class="panel-title-sign mt-xl text-right">
-                <h2 class="title text-uppercase text-bold m-none"><i class="fa fa-user mr-xs"></i> Sign In</h2>
+<section class="material-half-bg">
+    <div class="cover"></div>
+</section>
+<section class="login-content">
+    <div class="logo">
+        <h1>{{ env( 'NAME_COMMERCIAL_PROJECT' ) }}</h1>
+    </div>
+    <div class="login-box">
+        <form class="login-form" action="{{ route('login') }}" method="post">
+            {{ csrf_field() }}
+            <h3 class="login-head"><i class="fa fa-lg fa-fw fa-user"></i>Ingreso al Sistema</h3>
+            <div class="form-group">
+                <label class="control-label">Correo</label>
+                <input class="form-control" name="email" id="email" type="email" placeholder="Email" autofocus value="{{ old( 'email' ) }}">
             </div>
-            <div class="panel-body">
-                <form action="{{ route('login') }}" method="post">
-                    {{ csrf_field() }}
-                    <div class="form-group mb-lg">
-                        <label>E-Mail</label>
-                        <div class="input-group input-group-icon">
-                            <input name="email" id="email" type="email" class="form-control input-lg" placeholder="Correo." value="{{ old( 'email' ) }}"/>
-                            <span class="input-group-addon">
-                                <span class="icon icon-lg">
-                                    <i class="fa fa-user"></i>
-                                </span>
-                            </span>
-                        </div>
-                    </div>
-
-                    <div class="form-group mb-lg">
-                        <div class="clearfix">
-                            <label class="pull-left">Password</label>
-                            <a href="pages-recover-password.html" class="pull-right">Lost Password?</a>
-                        </div>
-                        <div class="input-group input-group-icon">
-                            <input name="password" id="password" type="password" class="form-control input-lg" />
-                            <span class="input-group-addon">
-                                <span class="icon icon-lg">
-                                    <i class="fa fa-lock"></i>
-                                </span>
-                            </span>
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="col-sm-8">
-                            <div class="checkbox-custom checkbox-default">
-                                <input id="RememberMe" name="rememberme" type="checkbox"/>
-                                <label for="RememberMe">Remember Me</label>
-                            </div>
-                        </div>
-                        <div class="col-sm-4 text-right">
-                            <button type="submit" class="btn btn-primary hidden-xs">Sign In</button>
-                            <button type="submit" class="btn btn-primary btn-block btn-lg visible-xs mt-lg">Sign In</button>
-                        </div>
-
-                    </div>
-
-                    <span class="mt-lg mb-lg line-thru text-center text-uppercase">
-                        <span>Visitanos</span>
-                    </span>
-                    <div class="mb-xs text-center">
-                        <a class="btn btn-primary mb-md ml-xs mr-xs" href="https://www.dpintart.com">{{ env( 'NAME_COMMERCIAL_PROJECT' ) }} <i class="fa fa-info-circle"></i></a>
-                    </div>
-                </form>
+            <div class="form-group">
+                <label class="control-label">Contraseña</label>
+                <input name="password" id="password" class="form-control" type="password" placeholder="Contraseña">
             </div>
-        </div>
-
-        <p class="text-center text-muted mt-md mb-md">&copy; Copyright {{ date('Y') }}. All Rights Reserved.</p>
+            <div class="form-group">
+                <div class="utility">
+                    <div class="animated-checkbox">
+                        <label>
+                            <input type="checkbox"><span class="label-text">Manténgase conectado</span>
+                        </label>
+                    </div>
+                    <p class="semibold-text mb-2"><a href="#" data-toggle="flip">¿Olvidó su contraseña?</a></p>
+                </div>
+            </div>
+            <div class="form-group btn-container">
+                <button class="btn btn-primary btn-block"><i class="fa fa-sign-in fa-lg fa-fw"></i>Iniciar Sesión</button>
+            </div>
+        </form>
     </div>
 </section>
-<!-- end: page -->
+<!-- Essential javascripts for application to work-->
+<script src="{{ URL::asset( 'js/jquery-3.2.1.min.js' ) }}"></script>
+<script src="{{ URL::asset( 'js/popper.min.js' ) }}"></script>
+<script src="{{ URL::asset( 'js/bootstrap.min.js' ) }}js/bootstrap.min.js"></script>
+<script src="{{ URL::asset( 'js/main.min.js' ) }}"></script>
+<!-- The javascript plugin to display page loading on top-->
+<script src="{{ URL::asset( 'js/plugins/pace.min.js' ) }}"></script>
+<script type="text/javascript">
+    // Login Page Flipbox control
+    $('.login-content [data-toggle="flip"]').click(function() {
+        $('.login-box').toggleClass('flipped');
+        return false;
+    });
+</script>
 </body>
 </html>
--- admin/webpack.mix.js
+++ admin/webpack.mix.js
@@ -13,201 +13,36 @@ const mix = require('laravel-mix');
 
 var resourceTemplate = 'resources/assets/';
 var template = resourceTemplate + 'plantilla/';
-var bootstrap = template + 'vendor/bootstrap/';
-var bootstrapMultiselect = template + 'vendor/bootstrap-multiselect/';
-var fontAwesome = template + 'vendor/font-awesome/';
-var magnificPopup = template + 'vendor/magnific-popup/';
-var skins = template + 'stylesheets/skins/';
-var modernizr = template + 'vendor/modernizr/';
-var jqueryDir = template + 'vendor/jquery/';
-var dirJBM = template + 'vendor/jquery-browser-mobile/';
-var nanoscroller = template + 'vendor/nanoscroller/';
-var bootstrapDatepicker = template + 'vendor/bootstrap-datepicker/';
-var jqueryPlaceholder = template + 'vendor/jquery-placeholder/';
-var jqueryUI = template + 'vendor/jquery-ui/';
-var jqueryUiTouchPunch = template + 'vendor/jquery-ui-touch-punch/';
-var jqueryAppear = template + 'vendor/jquery-appear/';
-var jqueryEasypiechart = template + 'vendor/jquery-easypiechart/';
-var jqueryFlot = template + 'vendor/flot/';
-var jqueryFlotTooltip = template + 'vendor/flot-tooltip/';
-var jquerySparkline = template + 'vendor/jquery-sparkline/';
-var raphael = template + 'vendor/raphael/';
-var morris = template + 'vendor/morris/';
-var gauge = template + 'vendor/gauge/';
-var snapSvg = template + 'vendor/snap-svg/';
-var liquidMeter = template + 'vendor/liquid-meter/';
-var jqvmap = template + 'vendor/jqvmap/';
-var javascripts = template + 'javascripts/';
+var resourceCss = template + 'css/';
+var resourceJS = template + 'js/';
+
+var plugins = resourceJS + 'plugins/';
 
 var publicPath = 'public/';
 var cssDist = publicPath + 'css/';
 var jsDist = publicPath + 'js/';
-
-var distBootstrap = cssDist + 'bootstrap/';
-var distFontAwesome = cssDist + 'font-awesome/';
-var distMagnificPopup = cssDist + 'magnific-popup/';
-var distJquery = jsDist + 'jquery/';
-var distJqueryCss = cssDist + 'jquery/';
-var distMorrisCss = cssDist + 'morris/';
-
-var distSkin = cssDist + 'skins/';
-var distModernizr = jsDist + 'modernizr/';
-var distBootstrapJS = jsDist + 'bootstrap/';
-var distNanoscroller = jsDist + 'nanoscroller/';
-var distMagnificPopupJS = jsDist + 'magnific-popup/';
-var distRaphaelJS = jsDist + 'raphael/';
-var distMorrisJS = jsDist + 'morris/';
-var distGaugeJS = jsDist + 'gauge/';
-var distSnapSvgJS = jsDist + 'snap-svg/';
-var distLiquidMeterJS = jsDist + 'liquid-meter/';
-var distThemeJS = jsDist + 'theme/';
-var development = jsDist + 'development/';
+var pluginsDist = jsDist + 'plugins/';
 
 var images = 'images/';
 
 mix.styles( [
-        bootstrap + 'css/bootstrap.css',
-    ], distBootstrap + 'bootstrap.min.css' )
-    .styles( [
-        jqueryUI + 'css/ui-lightness/jquery-ui-1.10.4.custom.css',
-    ], distJqueryCss + 'jquery-ui-1.10.4.custom.min.css' )
-    .styles( [
-        bootstrapMultiselect + 'bootstrap-multiselect.css',
-    ], distBootstrap + 'bootstrap-multiselect.min.css' )
-    .styles( [
-        morris + 'morris.css',
-    ], distMorrisCss + 'morris.min.css' )
-    .styles( [
-        fontAwesome + 'css/font-awesome.css'
-    ], distFontAwesome + 'style/font-awesome.min.css' )
-    .styles( [
-        magnificPopup + 'magnific-popup.css'
-    ], distMagnificPopup + 'magnific-popup.min.css' )
-    .styles( [
-        bootstrapDatepicker + 'css/datepicker3.css'
-    ], distBootstrap + 'datepicker3.min.css' )
-    .styles( [
-        template + 'stylesheets/theme.css'
-    ], cssDist + 'theme.min.css' )
-    .styles( [
-        skins + 'default.css'
-    ], distSkin + 'default.min.css' )
+        resourceCss + 'main.css',
+    ], cssDist + 'main.min.css' )
     .scripts( [
-        modernizr + 'modernizr.js'
-    ], distModernizr + 'modernizr.min.js' ).sourceMaps()
+        resourceJS + 'jquery-3.2.1.min.js'
+    ], jsDist + 'jquery-3.2.1.min.js' ).sourceMaps()
     .scripts( [
-        jqueryDir + 'jquery.js'
-    ], distJquery + 'jquery.min.js' ).sourceMaps()
+        resourceJS + '/bootstrap.min.js'
+    ], jsDist + 'bootstrap.min.js' ).sourceMaps()
     .scripts( [
-        dirJBM + 'jquery.browser.mobile.js'
-    ], distJquery + 'jquery.browser.mobile.min.js' ).sourceMaps()
+        resourceJS + 'popper.min.js'
+    ], jsDist + 'popper.min.js' ).sourceMaps()
     .scripts( [
-        bootstrap + '/js/bootstrap.js'
-    ], distBootstrapJS + 'bootstrap.min.js' ).sourceMaps()
+        plugins + 'pace.min.js'
+    ], pluginsDist + 'pace.min.js' ).sourceMaps()
     .scripts( [
-        nanoscroller + 'nanoscroller.js'
-    ], distNanoscroller + 'nanoscroller.min.js' ).sourceMaps()
-    .scripts( [
-        bootstrapDatepicker + 'js/bootstrap-datepicker.js'
-    ], distBootstrapJS + 'bootstrap-datepicker.min.js' ).sourceMaps()
-    .scripts( [
-        magnificPopup + 'magnific-popup.js'
-    ], distMagnificPopupJS + 'magnific-popup.min.js' ).sourceMaps()
-    .scripts( [
-        jqueryPlaceholder + 'jquery.placeholder.js'
-    ], distJquery + 'jquery.placeholder.min.js' ).sourceMaps()
-    .scripts( [
-        jqueryUI + 'js/jquery-ui-1.10.4.custom.js'
-    ], distJquery + 'query-ui-1.10.4.custom.min.js' ).sourceMaps()
-    .scripts( [
-        jqueryUiTouchPunch + 'jquery.ui.touch-punch.js'
-    ], distJquery + 'jquery.ui.touch-punch.min.js' ).sourceMaps()
-    .scripts( [
-        jqueryUiTouchPunch + 'jquery.appear.js'
-    ], distJquery + 'jquery.appear.min.js' ).sourceMaps()
-    .scripts( [
-        jqueryAppear + 'jquery.appear.js'
-    ], distJquery + 'jquery.appear.min.js' ).sourceMaps()
-    .scripts( [
-        bootstrapMultiselect + 'bootstrap-multiselect.js'
-    ], distBootstrapJS + 'bootstrap-multiselect.min.js' ).sourceMaps()
-    .scripts( [
-        jqueryEasypiechart + 'jquery.easypiechart.js'
-    ], distJquery + 'jquery.easypiechart.min.js' ).sourceMaps()
-    .scripts( [
-        jqueryFlot + 'jquery.flot.js'
-    ], distJquery + 'jquery.flot.min.js' ).sourceMaps()
-    .scripts( [
-        jqueryFlotTooltip + 'jquery.flot.tooltip.js'
-    ], distJquery + 'jquery.flot.tooltip.min.js' ).sourceMaps()
-    .scripts( [
-        jqueryFlot + 'jquery.flot.pie.js'
-    ], distJquery + 'jquery.flot.pie.min.js' ).sourceMaps()
-    .scripts( [
-        jqueryFlot + 'jquery.flot.categories.js'
-    ], distJquery + 'jquery.flot.categories.min.js' ).sourceMaps()
-    .scripts( [
-        jqueryFlot + 'jquery.flot.resize.js'
-    ], distJquery + 'jquery.flot.resize.min.js' ).sourceMaps()
-    .scripts( [
-        jquerySparkline + 'jquery.sparkline.js'
-    ], distJquery + 'jquery.sparkline.min.js' ).sourceMaps()
-    .scripts( [
-        raphael + 'raphael.js'
-    ], distRaphaelJS + 'raphael.min.js' ).sourceMaps()
-    .scripts( [
-        morris + 'morris.js'
-    ], distMorrisJS + 'morris.min.js' ).sourceMaps()
-    .scripts( [
-        gauge + 'gauge.js'
-    ], distGaugeJS + 'gauge.min.js' ).sourceMaps()
-    .scripts( [
-        snapSvg + 'snap.svg.js'
-    ], distSnapSvgJS + 'snap.svg.min.js' ).sourceMaps()
-    .scripts( [
-        liquidMeter + 'liquid.meter.js'
-    ], distLiquidMeterJS + 'liquid.meter.min.js' ).sourceMaps()
-    .scripts( [
-        jqvmap + 'jquery.vmap.js'
-    ], distJquery + 'jquery.vmap.min.js' ).sourceMaps()
-    .scripts( [
-        jqvmap + 'data/jquery.vmap.sampledata.js'
-    ], distJquery + 'jquery.vmap.sampledata.min.js' ).sourceMaps()
-    .scripts( [
-        jqvmap + 'maps/jquery.vmap.world.js'
-    ], distJquery + 'vmap.world.min.js' ).sourceMaps()
-    .scripts( [
-        jqvmap + 'maps/continents/jquery.vmap.africa.js'
-    ], distJquery + 'jquery.vmap.africa.min.js' ).sourceMaps()
-    .scripts( [
-        jqvmap + 'maps/continents/jquery.vmap.asia.js'
-    ], distJquery + 'jquery.vmap.asia.min.js' ).sourceMaps()
-    .scripts( [
-        jqvmap + 'maps/continents/jquery.vmap.australia.js'
-    ], distJquery + 'jquery.vmap.australia.min.js' ).sourceMaps()
-    .scripts( [
-        jqvmap + 'maps/continents/jquery.vmap.europe.js'
-    ], distJquery + 'jquery.vmap.europe.min.js' ).sourceMaps()
-    .scripts( [
-        jqvmap + 'maps/continents/jquery.vmap.north-america.js'
-    ], distJquery + 'jquery.vmap.north-america.min.js' ).sourceMaps()
-    .scripts( [
-        jqvmap + 'maps/continents/jquery.vmap.south-america.js'
-    ], distJquery + 'jquery.vmap.south-america.min.js' ).sourceMaps()
-    .scripts( [
-        javascripts + 'theme.js'
-    ], distThemeJS + 'theme.min.js' ).sourceMaps()
-    .scripts( [
-        javascripts + 'theme.custom.js'
-    ], distThemeJS + 'theme.custom.min.js' ).sourceMaps()
-    .scripts( [
-        javascripts + 'theme.init.js'
-    ], distThemeJS + 'theme.init.min.js' ).sourceMaps()
-    .scripts( [
-        javascripts + 'dashboard/examples.dashboard.js'
-    ], development + 'dashboard.min.js' ).sourceMaps()
-    .copyDirectory( bootstrap + 'fonts/', distBootstrap + 'fonts/')
-    .copyDirectory( fontAwesome + 'fonts/', distFontAwesome + 'fonts/')
+        resourceJS + 'main.js'
+    ], jsDist + 'main.min.js' ).sourceMaps()
     .copyDirectory( [
             template + images + 'logo.png',
             template + images + '!logged-user.jpg',
