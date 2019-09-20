1669c1b20fde22f83719d4e5232a77a09fa0aba9
LBROWNBR <bartoloricsi@gmail.com>
2019-09-20 01:22:13 -0500

    seeder productos y cotizacion

--- admin/database/migrations/2019_04_01_112656_create_products_table.php
+++ admin/database/migrations/2019_04_01_112656_create_products_table.php
@@ -30,43 +30,7 @@ class CreateProductsTable extends Migration
             $table->index('user_reg');
         });
 
-        $insert = [
-            [
-                'id' => 1,
-                'category_id' => 1,
-                'user_reg' => 1,
-                'name' => 'PUERTA DE MADERA NORMAL',                
-                'description' => 'PUERTA DE MADERA 2M X 1M',
-                'slug' => 'PUERTA-MADERA-2M-X-1M',  
-                'status' => 1,
-                'created_at' => date('Y-m-d H:i:s'),
-                'updated_at' => date('Y-m-d H:i:s')
-            ],
-            [
-                'id' => 2,
-                'category_id' => 1,
-                'user_reg' => 1,
-                'name' => 'PUERTA DE MELAMINA NORMAL',                
-                'description' => 'PUERTA DE MELAMINA 2M X 1M',
-                'slug' => 'PUERTA-MELAMINA-2M-X-1M',  
-                'status' => 1,
-                'created_at' => date('Y-m-d H:i:s'),
-                'updated_at' => date('Y-m-d H:i:s')
-            ],
-            [
-                'id' => 3,
-                'category_id' => 1,
-                'user_reg' => 1,
-                'name' => 'PUERTA DE TRIPLAY NORMAL',                
-                'description' => 'PUERTA DE TRIPLAY 2M X 1M',
-                'slug' => 'PUERTA-TRIPLAY-2M-X-1M',  
-                'status' => 1,
-                'created_at' => date('Y-m-d H:i:s'),
-                'updated_at' => date('Y-m-d H:i:s')
-            ]
-        ];
-
-        DB::table('products')->insert( $insert );
+        
     }
 
     /**
