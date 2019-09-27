eaf98da3576079e62846b7d5cf5af98fe8273c73
Julio Salsavilca <j.salsavilca@gmail.com>
2019-09-26 22:05:25 -0500

    [admin:seeder] Fix seeder presentation

--- admin/database/seeds/PresentationSeeder.php
+++ admin/database/seeds/PresentationSeeder.php
@@ -14,7 +14,7 @@ class PresentationSeeder extends Seeder
     public function run()
     {
         $insert = [];
-        $count = 0;
+        $count = DB::table( 'presentation' )->count() + 1;
 
         while( $count < 150 ) {
             $name = Str::random( 10 );
@@ -22,6 +22,7 @@ class PresentationSeeder extends Seeder
                 'products_id' => rand(1, 50),
                 'unity_id' => rand( 1, 7),
                 'sku' => 'PRO-PRUE-00' . ( $count +1 ),
+                'sku' => uniqid( 'PRO-PRUE-00' ),
                 'slug' => Str::slug( $name ),
                 'description' => $name,
                 'stock' => rand( 1, 100),
