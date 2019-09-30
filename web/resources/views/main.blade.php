1948eebe1aa46ea9c8d81e2e4e0e23ecc586dff3
Julio Salsavilca <j.salsavilca@gmail.com>
2019-09-30 16:10:37 -0500

    [WEB] delete app web

--- web/resources/views/main.blade.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace( '_', '-', app()->getLocale() ) }}">
-    @include('inc.head')
-    <body class="body-wrapper">
-        @include('inc.sidebar')
-        @yield('content')
-        @include('inc.footer')
-    </body>
-</html>
