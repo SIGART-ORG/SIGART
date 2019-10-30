35023bc4d045d4ea1dc0de02a7c58fffa26d17c7
LBROWNBR <bartoloricsi@gmail.com>
2019-10-30 00:30:01 -0500

    Modificación de la tabla productos e insercion de registros de la tabla productos

--- admin/database/seeds/ProductsTableSeeder.php
+++ admin/database/seeds/ProductsTableSeeder.php
@@ -16,28 +16,60 @@ class ProductsTableSeeder extends Seeder
         $insert = [];
         $count = 0;
 
-        $name = Str::random(20);
-        $insert[] = [
-            'category_id' => 1,
-            'user_reg' => rand(1, 52),
-            'name' => 'Otros Servicios',
-            'description' => 'Otros Servicios',
-            'slug' => Str::slug( $name ),
-            'created_at' => date( 'Y-m-d H:i:s' ),
-            'updated_at' => date( 'Y-m-d H:i:s' )
-        ];
+        
+        
 
         while ( $count < 50 ){
+
             $name = Str::random(20);
-            $insert[] = [
-                'category_id' => rand( 1, 9 ),
-                'user_reg' => rand(1, 52),
-                'name' => $name,
-                'description' => Str::random(30),
-                'slug' => Str::slug( $name ),
-                'created_at' => date( 'Y-m-d H:i:s' ),
-                'updated_at' => date( 'Y-m-d H:i:s' )
-            ];
+
+            if($count==0){
+                
+                $insert[] = [
+                    'category_id' => 1,
+                    'user_reg' => rand(1, 52),
+                    'name' => 'Otros Servicios',
+                    'description' => 'Otros Servicios',
+                    'slug' => Str::slug( $name ),
+                    'created_at' => date( 'Y-m-d H:i:s' ),
+                    'updated_at' => date( 'Y-m-d H:i:s' ),
+                    'cod_type_service' => '1',
+                ];
+
+            }else if($count==1){
+
+                $insert[] = [
+                    'category_id' => 2,
+                    'user_reg' => rand(1, 52),
+                    'name' => 'Otros Servicios',
+                    'description' => 'Otros Servicios',
+                    'slug' => Str::slug( $name ),
+                    'created_at' => date( 'Y-m-d H:i:s' ),
+                    'updated_at' => date( 'Y-m-d H:i:s' ),
+                    'cod_type_service' => '2',
+                ];
+
+            }else{
+
+                if (($count % 2) == 1){
+                    $tipo = '1';
+                }else{
+                    $tipo = '2';
+                }
+
+                $insert[] = [
+                    'category_id' => rand( 1, 9 ),
+                    'user_reg' => rand(1, 52),
+                    'name' => $name,
+                    'description' => Str::random(30),
+                    'slug' => Str::slug( $name ),
+                    'created_at' => date( 'Y-m-d H:i:s' ),
+                    'updated_at' => date( 'Y-m-d H:i:s' ),
+                    'cod_type_service' => $tipo,
+                ];
+
+
+            }
 
             $count++;
         }
